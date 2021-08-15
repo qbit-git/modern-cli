f80e10e84be223d9a022e4ef8383aa71026afa4f
qbit-git <61911047+qbit-git@users.noreply.github.com>
2021-08-15 22:50:11 +0800

    Add files via upload

---
+++ modern_cli_ubuntu2004.sh
@@ -0,0 +1,56 @@
+sudo apt update
+
+# rg <- grep (Rust)
+# https://github.com/BurntSushi/ripgrep
+sudo apt install ripgrep -y
+sudo sed -i '/\/usr\/.crates2.json/d' /var/lib/dpkg/info/ripgrep.list  # solve conflict witch bat
+echo 'alias grep=rg' >> ~/.bashrc 
+
+# bat <- cat (Rust)
+# https://github.com/sharkdp/bat
+sudo apt install bat -y
+echo 'alias cat=batcat' >> ~/.bashrc 
+
+# fd <- find (Rust)
+# https://github.com/sharkdp/fd
+sudo install fd-find -y
+echo 'alias fd=fdfind' >> ~/.bashrc 
+echo 'alias find="fdfind -H"' >> ~/.bashrc 
+
+# exa <- ls (Rust)
+# Only Ubuntu 20.10 (Groovy Gorilla) and later
+# https://github.com/ogham/exa
+# sudo apt install exa -y
+# echo 'alias ls=exa' >> ~/.bashrc 
+
+# procs <- ps (Rust)
+# https://github.com/dalance/procs
+# maybe later
+
+# sd <- sed (Rust)
+# https://github.com/chmln/sd
+# maybe later
+
+# dust <- du (Rust)
+# https://github.com/bootandy/dust
+# maybe later
+
+# starship (Rust)
+# https://github.com/starship/starship
+# maybe later
+
+# bandwhich <- nethogs (Rust)
+# https://github.com/imsnif/bandwhich
+# maybe later
+
+# pipx
+# https://github.com/pypa/pipx
+pip3 install pipx
+echo 'export PATH=$PATH:/home/ubuntu/.local/bin' >> ~/.bashrc 
+
+# tldr 
+# https://github.com/tldr-pages/tldr-python-client
+pipx install tldr
+tldr -u
+
+source ~/.bashrc
